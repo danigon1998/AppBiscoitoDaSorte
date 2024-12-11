b483fb34c6da6e930a3d69d63c2dc29c0a3891ff
Daniel <danigon1998@gmail.com>
2024-12-10 23:05:52 -0300

    Criado app do biscoito da sorte

--- App.tsx
+++ App.tsx
@@ -1,118 +1,111 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {Component} from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity,TextInput, Button } from "react-native";
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+interface IAppProps{
+  
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+interface IAppState{
+  textoFrase: string;
+  img: any;
+}
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const FRASES = [
+  'A vida trará coisas boas se tiver paciência.',
+  'Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.',
+  'Não compense na ira o que lhe falta na razão.',
+  'Defeitos e virtudes são apenas dois lados da mesma moeda.',
+  'A maior de todas as torres começa no solo.',
+  'Não há que ser forte. Há que ser flexível.',
+  'São os nossos amigos que nos ensinam as mais valiosas lições.',
+  'Uma bela flor é incompleta sem as suas folhas.',
+  'Sem o fogo do entusiasmo, não há o calor da vitória.',
+  'O riso é a menor distância entre duas pessoas.',
+  'A sorte favorece a mente bem preparada.',
+  'A paciência na adversidade é sinal de um coração zangado.',
+  'A verdadeira viúva é uma paraiba.',
+  'O amor é a alegria mais bela das alegrias.',
+  'A esperança é a alegria mais bela das alegrias.',
+  'A paciência é a alegria mais bela das alegrias.',
+  'A esperança é a alegria mais bela das alegrias.',
+  'A esperança é a alegria mais bela das alegrias.',
+]
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+class App extends Component<IAppProps,IAppState>{
+
+  constructor(props:IAppProps){
+    super(props);
+    this.state = {
+      textoFrase:'',
+      img: require('./src/biscoito.png')
+    };
+    this.handleMudarFrase = this.handleMudarFrase.bind(this);
+  }
+
+  handleMudarFrase(){
+    let numeroAleatorio = Math.floor(Math.random() * FRASES.length);
+    this.setState({textoFrase: FRASES[numeroAleatorio]});
+    this.setState({img: require('./src/biscoitoAberto.png')});
+  }
+
+  render(): React.ReactNode {
+
+    return(
+      <View style={styles.container}>
+        <Image
+        source={this.state.img}
+        style={styles.img}/>
+        <Text style={styles.textoFrase}>
+          {this.state.textoFrase}
+        </Text>
+        <TouchableOpacity style={styles.botao} onPress={this.handleMudarFrase}>
+          <View style={styles.btnArea}>
+            <Text style={styles.btnTexto}>
+              Quebre Biscoito
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      );
+  }
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    paddingTop:20,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  img:{
+    width:250,
+    height:250
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  textoFrase:{
+    fontSize:20,
+    color: '#dd7b22',
+    margin:30,
+    fontStyle: 'italic',
+    textAlign: 'center'
   },
-  highlight: {
-    fontWeight: '700',
+  botao:{
+    width:230,
+    height:50,
+    borderColor:'#dd7b22',
+    borderWidth:2,
+    borderRadius:25
   },
+  btnArea:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  btnTexto:{
+    fontSize:18,
+    fontWeight:'bold',
+    color:'#dd7b22'
+  }
+
 });
 
 export default App;
